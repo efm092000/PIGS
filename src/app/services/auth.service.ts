4ad073830b1d7389dbac5c63d6c102457155bd16
Chamil <chamil.studyacc@gmail.com>
2025-04-05 02:04:57 +0100

    fix: refactoring

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,35 +1,32 @@
 import { Injectable } from '@angular/core';
-import {
-  getAuth,
-  signInWithEmailAndPassword,
-  createUserWithEmailAndPassword,
-  signOut,
-} from 'firebase/auth';
-import { Observable, of } from 'rxjs';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import firebase from '@firebase/app-compat';
 import { User } from 'firebase/auth';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  auth = getAuth();
-
-  constructor() {}
+  constructor(private _auth: AngularFireAuth) {}
 
   getUser(): Observable<User | null> {
-    const user = this.auth.currentUser;
-    return of(user);
+    return this._auth.authState as Observable<User | null>;
   }
 
-  login(email: string, password: string): Promise<any> {
-    return signInWithEmailAndPassword(this.auth, email, password);
+  signup(email: string, password: string) {
+    return this._auth.createUserWithEmailAndPassword(email, password);
   }
 
-  register(email: string, password: string): Promise<any> {
-    return createUserWithEmailAndPassword(this.auth, email, password);
+  login(email: string, password: string) {
+    return this._auth.signInWithEmailAndPassword(email, password);
   }
 
-  logout(): Promise<void> {
-    return signOut(this.auth);
+  logout() {
+    return this._auth.signOut();
+  }
+
+  register(email: string, password: string) {
+    return this._auth.createUserWithEmailAndPassword(email, password);
   }
 }
