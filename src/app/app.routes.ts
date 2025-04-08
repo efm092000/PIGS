12ba7c5c831ef92cc8d9700d3a2e6cb27bc1dda8
efm092000 <efm092000@gmail.com>
2025-04-08 20:12:25 +0100

    Reinstalled friebase

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,7 +6,8 @@ import { RegisterComponent } from './pages/register/register.component';
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: '', redirectTo: '', pathMatch: 'full' }
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'homepage', component: LoginComponent}
 ];
 
 @NgModule({
--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,13 @@ import { importProvidersFrom } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app/app-routing.module';
-import { HttpClientModule } from '@angular/common/http'; // <-- IMPORTA ESTO
+import { HttpClientModule } from '@angular/common/http';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore'; // <-- IMPORTA ESTO
 
 bootstrapApplication(AppComponent, {
   providers: [
-    importProvidersFrom(BrowserModule, FormsModule, AppRoutingModule, HttpClientModule) // <-- AGREGA HttpClientModule
+    importProvidersFrom(BrowserModule, FormsModule, AppRoutingModule, HttpClientModule), provideFirebaseApp(() => initializeApp({ projectId: "pigs-d972f", appId: "1:171180235442:web:50c7a7a56c58e7e0b947ab", storageBucket: "pigs-d972f.firebasestorage.app", apiKey: "AIzaSyAlGmCCS60ao6DrfV91y7bGY3kYw_-vt88", authDomain: "pigs-d972f.firebaseapp.com", messagingSenderId: "171180235442", measurementId: "G-DJC723FGMX" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()) // <-- AGREGA HttpClientModule
   ]
 }).catch(err => console.error(err));
