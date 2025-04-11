1dff3988aa32b3d73da89297fcf295a28cded2af
efm092000 <efm092000@gmail.com>
2025-04-11 09:58:46 +0100

    Fixed problems in the register page

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
@@ -9,20 +10,34 @@ import { CommonModule } from '@angular/common';
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css'],
 })
+
 export class RegisterComponent {
-  email: string = '';
-  password: string = '';
-  confirmPassword: string = '';
-  errorMessage: string = '';
+  email = '';
+  password = '';
+  confirmPassword = '';
+  errorMessage = '';
 
-  constructor(private router: Router) {}
-
-  navigateToLogin() {
-    this.router.navigate(['/login']);
-  }
+  constructor(private authService: AuthService, private router: Router) {}
 
   register() {
-    // Lógica para registrar al usuario
-    console.log('User registered:', this.email, this.password);
+    if (this.password !== this.confirmPassword) {
+      this.errorMessage = 'Passwords do not match!';
+      return;
+    }
+
+    if (this.email && this.password) {
+      this.authService
+        .register(this.email, this.password)
+        .then((userCredential) => {
+          console.log('User registered:', userCredential);
+          this.router.navigate(['/home']);
+        })
+        .catch((error) => {
+          this.errorMessage = error.message;
+          console.error('Registration failed:', error);
+        });
+    } else {
+      this.errorMessage = 'Please enter both email and password';
+    }
   }
 }
