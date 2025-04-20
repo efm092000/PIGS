f450b6f9777a4d7d317a06c8ac1afac32cf5c7e0
itzha <itzhakprieto@gmail.com>
2025-04-20 20:46:59 +0100

    Integration of the catalog frontend with the backend and style updates

--- src/app/root/app/app.routes.ts
+++ src/app/root/app/app.routes.ts
@@ -4,11 +4,13 @@ import { HomeComponent } from '../../pages/home/home.component';
 import { LoginComponent } from '../../pages/login/login.component';
 import { RegisterComponent } from '../../pages/register/register.component';
 import { ShipFormComponent } from '../../components/ship-form/ship-form.component';
+import { CatalogComponent } from '../../pages/catalog/catalog.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '', redirectTo: '/catalog', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'home', component: HomeComponent },
-  { path: 'form', component: ShipFormComponent}
+  { path: 'form', component: ShipFormComponent},
+  {path: 'catalog', component:CatalogComponent}
 ];
