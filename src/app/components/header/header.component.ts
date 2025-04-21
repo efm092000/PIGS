3a1f9d6756125f28004893550d710407b4ec4d5b
AVR21 <alejandroviera12@gmail.com>
2025-04-21 12:21:02 +0100

    feat: added login routing & logout feature within profile pic in header component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router} from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -7,8 +8,24 @@ import { Router} from '@angular/router';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
-  constructor(private router: Router){}
+export class HeaderComponent implements OnInit{
+  isAuthenticated: Boolean = false;
+
+  constructor(private authService: AuthService, private router: Router){}
+  
+  ngOnInit(): void {
+    this.authService.getUser().subscribe((user) => {
+      this.isAuthenticated = !!user; // Set to true if the user exists, false otherwise
+    });
+  }
+
+  userLogout(){
+    this.authService.logout();
+    this.authService.getUser().subscribe((user) => {
+      this.isAuthenticated = !!user; // Set to true if the user exists, false otherwise
+    });
+  }
+
   cargarPagina(url: string) {
     this.router.navigate([url]);
     localStorage.setItem('returnUrl', this.router.url);
