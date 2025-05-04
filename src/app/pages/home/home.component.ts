4c07c2c364fbafd632f685d9577bae828ccc5f14
Chamil <chamil.studyacc@gmail.com>
2025-05-04 05:19:38 +0100

    debug(home, header): ui improvements

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,24 +1,58 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../../services/auth/auth.service';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../../components/header/header.component';
 import { FooterComponent } from '../../components/footer/footer.component';
+import { SelectService } from '../../services/select/select.service';
+import { FirestoreApiService } from '../../services/firestore-api/firestore-api.service';
+import { Vessel } from '../../interfaces/vessel';
+import { VesselCardComponent } from '../../components/vessel-card/vessel-card.component';
+import { CommonButtonComponent } from '../../components/common-button/common-button.component';
 
 @Component({
-  imports: [CommonModule, HeaderComponent, FooterComponent],
+  imports: [
+    CommonModule,
+    HeaderComponent,
+    FooterComponent,
+    VesselCardComponent,
+    CommonButtonComponent,
+  ],
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  isAuthenticated: boolean = false; // Track if the user is authenticated
+  vessels: Vessel[] = [];
+  randomVessels: Vessel[] = []; // Array to store 3 random vessels
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(
+    private router: Router,
+    private selectService: SelectService,
+    private firestore: FirestoreApiService
+  ) {}
 
   ngOnInit(): void {
-    this.authService.getUser().subscribe((user) => {
-      this.isAuthenticated = !!user; // Set to true if the user exists, false otherwise
+    this.firestore.getVessels().subscribe((data) => {
+      this.vessels = data;
+      this.pickRandomVessels();
     });
+  }
+
+  // Method to pick 3 random vessels from the available vessels
+  pickRandomVessels(): void {
+    if (this.vessels.length >= 3) {
+      const shuffled = this.vessels.sort(() => 0.5 - Math.random()); // Shuffle the array
+      this.vessels = shuffled.slice(0, 3); // Take the first 3 vessels
+    }
+  }
+
+  selectVessel(vessel: Vessel): void {
+    this.selectService.selectCard(vessel);
+    this.router.navigate(['/select']);
+  }
+
+  navigateTo(url: string) {
+    this.router.navigate([url]);
+    localStorage.setItem('returnUrl', this.router.url);
   }
 }
